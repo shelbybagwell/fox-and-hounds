7fb1f5bb745c48e0e2d7ae526249fd7ac06e07da
Shelby Bagwell <sbagwel2@vols.utk.edu>
2025-09-29 20:44:26 -0500

    moves logic

--- gameboard/board.py
+++ gameboard/board.py
@@ -10,6 +10,8 @@ class GameBoard:
         # set first turn to fox
         self.board_size = 6
         self.board = self._make_board()
+        self.current_player = "FOX"
+        self.game_over = False
 
     def _make_board(self):
         # 6x6 board
@@ -36,10 +38,49 @@ class GameBoard:
         # switch player turn
         return
 
+    def get_piece_moves(self, r, c):
+        moves = []
+        piece = self.board[r, c]
+
+        # Fox logic
+        if piece == 2:
+            dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        # Hound logic
+        elif piece == 1:
+            dirs = [(-1, 1), (-1, 1)]
+        else:
+            # something went wrong??
+            print("ERROR: invalid piece type")
+            return []
+
+        for dr, dc in dirs:
+            nr, nc = r + dr, c + dc
+            if (
+                0 <= nr < self.board_size  # valid row
+                and 0 <= nc < self.board_size  # valid column
+                and self.board[nr, nc] == 0  # space is unoccupied
+            ):
+                moves.append((nr, nc))
+
+        return moves
+
     def get_available_moves(self, player):
         # get all valid moves on board for specified player
-        # may not need if AIs handle this logic
-        return
+        all_moves = []
+
+        if self.current_player == "FOX":
+            piece_val = 2
+        else:
+            piece_val = 1
+
+        cur_coords = np.argwhere(self.board == piece_val)
+
+        for r, c in cur_coords:
+            piece_moves = self.get_piece_moves(r, c)
+            for end_pos in piece_moves:
+                all_moves.append(((r, c), end_pos))
+
+        return all_moves
 
     def check_win(self):
         # to be run after each move
